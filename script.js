2e0b188a3c7fd56037f8e42838f3d2d5df56a641
Bryce Large <bryce@jiro.co.za>
2015-06-24 16:23:50 +0200

    Added new add location type session ajax updater, changed from jQuery live to on

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 jQuery(document).ready(function($) {
+	add_location_type_to_session();
+
+	var timer_update_billing_location_type;
+	var timer_update_shipping_location_type;
 	var timer_update_billing_subs;
 	var timer_update_shipping_subs;
+	var mds_ajax_location_type;
 	var mds_ajax_billing_state;
 	var mds_ajax_shipping_state;
 
@@ -73,12 +78,58 @@ jQuery(document).ready(function($) {
 		}
 	}
 
+	function add_location_type_to_session() {
+		if (mds_ajax_location_type) {
+			mds_ajax_billing_state.abort();
+		}
+
+		var shipping_location_type = jQuery('#shipping_location_type').val();
+		var billing_location_type = jQuery('#billing_location_type').val();
+
+		if($("input[name='ship-to-different-address']").prop("checked")) {
+			var use_location_type = 'shipping_location_type';
+		} else {
+			var use_location_type = 'billing_location_type';
+		}
+
+		var data = {
+			'action': 'add_location_type_to_session',
+			'use_location_type': use_location_type,
+			'shipping_location_type': shipping_location_type,
+			'billing_location_type': billing_location_type
+		};
+
+		mds_ajax_location_type = jQuery.ajax({
+			url: woocommerce_params.ajax_url,
+			data: data,
+			type: 'post'
+		});
+	}
+
+	jQuery('#shipping_location_type').on('keydown', function(e) {
+		var keyCode = e.keyCode || e.which;
+
+		if (keyCode != 9) {
+			clearTimeout(timer_update_shipping_location_type);
+			timer_update_billing_subs = setTimeout(add_location_type_to_session, '2000');
+		}
+	});
+
+	jQuery('#billing_location_type').on('keydown', function(e) {
+		var keyCode = e.keyCode || e.which;
+
+		if (keyCode != 9) {
+			clearTimeout(timer_update_billing_location_type);
+			timer_update_billing_subs = setTimeout(add_location_type_to_session, '2000');
+		}
+	});
+
 	jQuery('#billing_state').on('keydown', function(e) {
 		var keyCode = e.keyCode || e.which;
 
 		if (keyCode != 9) {
 			clearTimeout(timer_update_billing_subs);
-			timer_update_billing_subs = setTimeout(update_billing_subs, '1000');
+			timer_update_billing_subs = setTimeout(update_billing_subs, '2000');
 		}
 	});
 
@@ -87,8 +138,18 @@ jQuery(document).ready(function($) {
 
 		if (keyCode != 9) {
 			clearTimeout(timer_update_shipping_subs);
-			timer_update_shipping_subs = setTimeout(update_shipping_subs, '1000');
+			timer_update_shipping_subs = setTimeout(update_shipping_subs, '2000');
 		}
+	});
+
+	jQuery('#shipping_location_type').on('change', function() {
+		clearTimeout(timer_update_shipping_location_type);
+		add_location_type_to_session();
+	});
+
+	jQuery('#billing_location_type').on('change', function() {
+		clearTimeout(timer_update_billing_location_type);
+		add_location_type_to_session();
 	});
 
 	jQuery('#billing_state').on('change', function() {
