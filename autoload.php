972cd5f25960330df62119542870a62a25d8964d
Bryce Large <bryce@jiro.co.za>
2015-10-14 15:42:36 +0200

    Added class Autoloader

---
+++ autoload.php
@@ -0,0 +1,29 @@
+<?php
+
+class MdsColliveryAutoLoader
+{
+	protected static $classMap = array(
+		'MdsCache' => '\MdsSupportingClasses\MdsCache',
+		'MdsCollivery' => '\MdsSupportingClasses\Collivery',
+		'GitHubPluginUpdater' => '\MdsSupportingClasses\GitHubPluginUpdater',
+		'MdsColliveryService' => '\MdsSupportingClasses\MdsColliveryService',
+		'ParseDown' => '\MdsSupportingClasses\ParseDown',
+		'UnitConverter' => '\MdsSupportingClasses\UnitConverter',
+	);
+
+	public static function autoload($class)
+	{
+		$classParts = explode('\\', $class);
+		$vendor = array_shift($classParts);
+		if ($vendor === 'MdsSupportingClasses') {
+			require _MDS_DIR_ . '/MdsSupportingClasses/' . implode('/', $classParts) . '.php';
+		} elseif (array_key_exists($class, self::$classMap)) {
+			class_alias(self::$classMap[$class], $class);
+		}
+	}
+}
+
+spl_autoload_register(
+	'MdsColliveryAutoLoader::autoload',
+	true
+);
