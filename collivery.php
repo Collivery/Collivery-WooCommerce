a81c12dda0cd7eeb4b249422d57adf141257642b
Bryce Large <bryce@jiro.co.za>
2016-08-24 12:07:47 +0200

    New version release to add the download button for log files

--- collivery.php
+++ collivery.php
@@ -1,14 +1,14 @@
 <?php
 
 define('_MDS_DIR_', __DIR__);
-define('MDS_VERSION', "2.3.7");
+define('MDS_VERSION', "2.3.8");
 include('autoload.php');
 
 /**
  * Plugin Name: MDS Collivery
  * Plugin URI: http://www.collivery.co.za/
  * Description: Plugin to add support for MDS Collivery in WooCommerce.
- * Version: 2.3.7
+ * Version: 2.3.8
  * Author: Bryce Large
  * License: GNU/GPL version 3 or later: http://www.gnu.org/licenses/gpl.html
  */
