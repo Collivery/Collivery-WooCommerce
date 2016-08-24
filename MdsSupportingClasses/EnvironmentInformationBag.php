384344a01c4070c3431345a1ced34efef8e3a647
Bryce Large <bryce@jiro.co.za>
2016-08-24 11:36:07 +0200

    Created new EnvironmentInformationBag class

---
+++ MdsSupportingClasses/EnvironmentInformationBag.php
@@ -0,0 +1,123 @@
+<?php namespace MdsSupportingClasses;
+
+class EnvironmentInformationBag
+{
+	/**
+	 * @var string
+	 */
+	public $wordpressVersion;
+
+	/**
+	 * @var string
+	 */
+	public $woocommerceVersion;
+
+	/**
+	 * @var string
+	 */
+	public $appName;
+
+	/**
+	 * @var string
+	 */
+	public $appVersion;
+
+	/**
+	 * @var string
+	 */
+	public $appHost;
+
+	/**
+	 * @var string
+	 */
+	public $appUrl;
+
+	/**
+	 * @var string
+	 */
+	public $phpVersion;
+
+	/**
+	 * @var string
+	 */
+	public $soapInstalled;
+
+	/**
+	 * @var array|null
+	 */
+	protected $settings;
+
+	/**
+	 * EnvironmentInformation constructor.
+	 * @param array $settings
+	 */
+	public function __construct(array $settings = null)
+	{
+		global $wp_version;
+
+		if($settings) {
+			$this->setSettings($settings);
+		}
+
+		$this->wordpressVersion = $wp_version;
+		$this->woocommerceVersion = $this->getWoocommerceVersionNumber();
+		$this->appHost = 'Wordpress: ' . $wp_version . ' - WooCommerce: ' . $this->wordpressVersion;
+		$this->appName = 'WooCommerce MDS Shipping Plugin';
+		$this->phpVersion = phpversion();
+		$this->appVersion = MDS_VERSION;
+		$this->appUrl = get_site_url();
+		$this->soapInstalled = (extension_loaded('soap')) ? 'yes' : 'no';
+	}
+
+	/**
+	 * @param array $settings
+	 */
+	public function setSettings(array $settings)
+	{
+		if(isset($settings['mds_pass'])) {
+			unset($settings['mds_pass']);
+		}
+
+		$this->settings = $settings;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function loggerFormat()
+	{
+		return array(
+			'wordpressVersion' => $this->wordpressVersion,
+			'woocommerceVersion' => $this->woocommerceVersion,
+			'appHost' => $this->appHost,
+			'appName' => $this->appName,
+			'phpVersion' => $this->phpVersion,
+			'appVersion' => $this->appVersion,
+			'appUrl' => $this->appUrl,
+			'soapInstalled' => $this->soapInstalled,
+		) + $this->settings;
+	}
+
+	/**
+	 * @return null
+	 */
+	private function getWoocommerceVersionNumber()
+	{
+		// If get_plugins() isn't available, require it
+		if (!function_exists('get_plugins')) {
+			require_once ABSPATH . 'wp-admin/includes/plugin.php';
+		}
+
+		// Create the plugins folder and file variables
+		$plugin_folder = get_plugins('/' . 'woocommerce');
+		$plugin_file = 'woocommerce.php';
+
+		// If the plugin version number is set, return it
+		if (isset($plugin_folder[$plugin_file]['Version'])) {
+			return $plugin_folder[$plugin_file]['Version'];
+		} else {
+			// Otherwise return null
+			return NULL;
+		}
+	}
+}
