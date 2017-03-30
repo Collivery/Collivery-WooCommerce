84a397dd7efd63f8241e2a08c05d216de87991ba
Bryce Large <bryce@jiro.co.za>
2017-03-30 16:07:52 +0200

    Updated my_custom_checkout_field_update_order_meta to save the new checkout fields

--- mds_checkout_fields.php
+++ mds_checkout_fields.php
@@ -16,7 +16,6 @@ if (!function_exists('custom_override_default_address_fields')) {
 	add_filter('woocommerce_default_address_fields', 'custom_override_default_address_fields');
 }
 
-
 if (!function_exists('custom_override_checkout_fields')) {
 	/**
 	 * Override the Billing and Shipping fields in Checkout
@@ -37,26 +36,28 @@ if (!function_exists('custom_override_checkout_fields')) {
 	add_filter('woocommerce_checkout_fields', 'custom_override_checkout_fields');
 }
 
-if (!function_exists('custom_override_state_label')) {
+if (!function_exists('my_custom_checkout_field_update_order_meta')) {
 	/**
-	 * Rename Province to Town/City
+	 * Save our location type field
 	 *
-	 * @param $locale
-	 * @return mixed
+	 * @param $order_id
 	 */
-	function custom_override_state_label($locale)
+	function my_custom_checkout_field_update_order_meta($order_id)
 	{
-		/** @var \MdsSupportingClasses\MdsColliveryService $mds */
-		$mds = MdsColliveryService::getInstance();
-		if (!$mds->isEnabled()) {
-			return $locale;
-		}
+		$mds = \MdsSupportingClasses\MdsColliveryService::getInstance();
 
-		$locale['ZA']['state']['label'] = __('Town/City');
-		return $locale;
+		if ($mds->isEnabled()) {
+			foreach(array('shipping_', 'billing_') as $prefix) {
+				foreach(array('suburb', 'location_type') as $field) {
+					if (!empty($_POST[$prefix . $field])) {
+						update_post_meta($order_id, $prefix . $field, sanitize_text_field($_POST[$prefix . $field]));
+					}
+				}
+			}
+		}
 	}
 
-	add_filter('woocommerce_get_country_locale', 'custom_override_state_label');
+	add_action('woocommerce_checkout_update_order_meta', 'my_custom_checkout_field_update_order_meta');
 }
 
 if (!function_exists('generate_towns')) {
