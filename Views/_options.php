267c293dbfe0eec021700616371670b43afed870
Bryce Large <bryce@jiro.co.za>
2017-03-30 16:03:44 +0200

    Added shared view for building select options

---
+++ Views/_options.php
@@ -0,0 +1,19 @@
+<?php
+if (!empty($placeholder)) {
+    if ((isset($selectedValue) || !isset($fields[$selectedValue])) || count($fields) > 1) {
+        echo '<option selected="selected">'.$placeholder.'</option>';
+    } else {
+        echo '<option>'.$placeholder.'</option>';
+    }
+}
+
+if (!empty($fields)) {
+    foreach ($fields as $value) {
+        if (count($fields) === 0 || (isset($selectedValue) && $selectedValue !== '' && $selectedValue === $value)) {
+            echo '<option value="'.$value.'" selected="selected">'.$value.'</option>';
+        } else {
+            echo '<option value="'.$value.'">'.$value.'</option>';
+        }
+    }
+}
+?>
