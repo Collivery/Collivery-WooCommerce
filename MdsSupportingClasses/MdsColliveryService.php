9f2c53a5da6b49855359d586a9c05df527a873ec
Peter Chaula <petercchaula@gmail.com>
2017-07-28 16:43:27 +0200

    Fix: Declare temporary variable before checking for empty
     Fixes Can't use method return value for PHP 5.4 and older

--- MdsSupportingClasses/MdsColliveryService.php
+++ MdsSupportingClasses/MdsColliveryService.php
@@ -449,7 +449,7 @@ class MdsColliveryService
             $id = $this->validated_data['service'];
             $services = $this->collivery->getServices();
 
-            if (!empty($this->settings->getValue("wording_$id"))) {
+            if (!$this->settings->getValue("wording_$id", true)) {
                 $reason = preg_replace('|' . preg_quote($services[$id]) . '|', $this->settings->getValue("wording_$id"), $this->validated_data['time_changed_reason']);
             } else {
                 $reason = $this->validated_data['time_changed_reason'];
