34d6fa957c28eddf19a76ea8d39eb334d9c74334
William Ryan <william@mdstech.co.za>
2018-11-29 10:23:41 +0200

    Use correct index for array

--- mds_admin.php
+++ mds_admin.php
@@ -566,7 +566,7 @@ function mds_register_collivery()
     $towns = $collivery->getTowns();
     $services = $collivery->getServices();
     $location_types = $collivery->getLocationTypes();
-    $suburbs = array(0 => 'Select Town');
+    $suburbs = array('' => 'Select Town');
     $populatedSuburbs =array_merge($suburbs,$collivery->getSuburbs(array_search($order->get_shipping_city(), $collivery->getTowns())));
 
     $shipping_method = null;
