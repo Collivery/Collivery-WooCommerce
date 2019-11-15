e05b33ed61e6bf3031c7ba6f02cea8a749f2d70f
Marnu Lombard <marnulombard@gmail.com>
2019-11-15 09:21:12 +0200

    EP-21 : [change] Minor clean up to MDS admin JS

--- Views/js/mds_collivery.js
+++ Views/js/mds_collivery.js
@@ -295,42 +295,44 @@ function form_validate() {
     }
 }
 
-// remove parcel
-function remove_parcel(id) {
-    jQuery('#item' + id).remove();
-}
+(function(jQuery){
+  jQuery.fn.hideParent = function(parent, hide){
+    if(hide === true)
+      this.parents(parent).fadeOut('fast');
+    else
+      this.parents(parent).fadeIn('fast');
 
-jQuery(function(){
-    var downloadSetting = jQuery("#woocommerce_mds_collivery_downloadLogs");
-    if(downloadSetting.length > 0) {
-        var url = downloadSetting.attr('placeholder');
-        var downloadButton = document.createElement('a');
-        downloadButton.setAttribute('href', url);
-        downloadButton.setAttribute('class', 'button-primary');
-        downloadButton.innerHTML = 'Download Error Logs';
-        downloadButton.setAttribute('id', 'woocommerce_mds_collivery_downloadLogs');
-        downloadSetting.replaceWith(downloadButton);
+    return this;
+  };
 
-        var clearCacheButton = document.createElement('a');
-        clearCacheButton.innerHTML = 'Clear Cache';
-        clearCacheButton.setAttribute('class', 'button-primary');
-        clearCacheButton.setAttribute('id', 'woocommerce_mds_collivery_clearCache');
-        clearCacheButton.setAttribute('href', url.replace('mds_download_log_files', 'mds_clear_cache_files'));
-        jQuery(clearCacheButton).css('margin-right', '10px').insertBefore('#woocommerce_mds_collivery_downloadLogs');
-    }
+})(jQuery);
 
-    (function(jQuery){
-        jQuery.fn.hideParent = function(parent, hide){
-            if(hide === true)
-                this.parents(parent).fadeOut('fast');
-            else
-                this.parents(parent).fadeIn('fast');
+// Download logs / Clear Cache
+jQuery(function() {
+  var downloadSetting = jQuery("#woocommerce_mds_collivery_downloadLogs");
+  if (downloadSetting.length > 0) {
+    var url = downloadSetting.attr('placeholder');
+    var downloadButton = document.createElement('a');
+    downloadButton.setAttribute('href', url);
+    downloadButton.setAttribute('class', 'button-primary');
+    downloadButton.innerHTML = 'Download Error Logs';
+    downloadButton.setAttribute('id', 'woocommerce_mds_collivery_downloadLogs');
+    downloadSetting.replaceWith(downloadButton);
 
-            return this;
-        };
+    var clearCacheButton = document.createElement('a');
+    clearCacheButton.innerHTML = 'Clear Cache';
+    clearCacheButton.setAttribute('class', 'button-primary');
+    clearCacheButton.setAttribute('id', 'woocommerce_mds_collivery_clearCache');
+    clearCacheButton.setAttribute('href',
+        url.replace('mds_download_log_files', 'mds_clear_cache_files'));
+    jQuery(clearCacheButton)
+        .css('margin-right', '10px')
+        .insertBefore('#woocommerce_mds_collivery_downloadLogs');
+  }
+})
 
-    })(jQuery);
-
+// Shipping mode show/hide
+jQuery(function() {
     var shippingMode = jQuery('select[name="woocommerce_mds_collivery_method_free"]');
     var percentageDiscount = jQuery('input[name="woocommerce_mds_collivery_shipping_discount_percentage"]');
     var freeDeliveryBlacklist = jQuery('input[name="woocommerce_mds_collivery_free_delivery_blacklist"]');
